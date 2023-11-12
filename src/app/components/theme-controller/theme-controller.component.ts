6b7e75cb1bc2643c3551873fe395daadb48c5178
Bernard Teye <datssosh@gmail.com>
2023-11-12 16:51:03 -0700

    add theme controller component to control the light and dark mode

---
+++ src/app/components/theme-controller/theme-controller.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-controller',
+  templateUrl: './theme-controller.component.html',
+  styleUrls: ['./theme-controller.component.css']
+})
+export class ThemeControllerComponent {
+
+}
