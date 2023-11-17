e045d0fec78da19d48516c77efcc0d634d52cc1f
Bernard Teye <datssosh@gmail.com>
2023-11-17 05:56:56 -0700

    configure routes to specific page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CountriesComponent } from './pages/countries/countries.component';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
+
+const routes: Routes = [
+  { path: "", component: CountriesComponent },
+  { path: "detail", component: CountryDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
