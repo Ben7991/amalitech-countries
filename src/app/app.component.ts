53bcfd398c107d90c6187a959dba0ce6a0d7a3b4
Bernard Teye <datssosh@gmail.com>
2023-11-17 06:06:30 -0700

    initialize theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeControllerService } from './shared/theme-controller.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project';
+export class AppComponent implements OnInit {
+
+  constructor(private themeController: ThemeControllerService) { }
+
+  ngOnInit(): void {
+    this.themeController.initializeTheme();
+  }
 }
