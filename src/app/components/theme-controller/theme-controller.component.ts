649af9ec4a0a9189ab0ec0653d31c2f9976051fe
Bernard Teye <datssosh@gmail.com>
2023-11-17 06:09:54 -0700

    move template code into component.ts file and removed the template file

--- src/app/components/theme-controller/theme-controller.component.ts
+++ src/app/components/theme-controller/theme-controller.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Theme, ThemeControllerService } from 'src/app/shared/theme-controller.service';
 
 @Component({
   selector: 'app-theme-controller',
-  templateUrl: './theme-controller.component.html',
-  styleUrls: ['./theme-controller.component.css']
+  styleUrls: ['./theme-controller.component.css'],
+  template: `
+    <div class="d-flex align-items-center gap-1 mode-text" role="button"
+      (click)="onToggleTheme()">
+      <span class="material-symbols-outlined">
+        dark_mode
+      </span>
+      <p class="m-0">Dark Mode</p>
+    </div>
+  `
 })
 export class ThemeControllerComponent {
 
+  constructor(private themeService: ThemeControllerService) { }
+
+  onToggleTheme() {
+    this.themeService.toggleTheme();
+  }
 }
