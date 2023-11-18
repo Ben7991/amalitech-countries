2024b8428ee251d276ec2982236cc9187dc938f4
Bernard Teye <datssosh@gmail.com>
2023-11-18 07:37:42 -0700

    change to hold all components added to complete the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { CountriesComponent } from './pages/countries/countries.component';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
+import { ThemeControllerComponent } from './components/theme-controller/theme-controller.component';
+import { CountryListComponent } from './pages/countries/country-list/country-list.component';
+import { CountryComponent } from './pages/countries/country-list/country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    CountriesComponent,
+    CountryDetailComponent,
+    ThemeControllerComponent,
+    CountryListComponent,
+    CountryComponent,
   ],
   imports: [
     BrowserModule,
