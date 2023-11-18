3b2221817bf93fea640de9746aea3ffebd8334cf
Bernard Teye <datssosh@gmail.com>
2023-11-18 07:33:23 -0700

    fetch specific country detail from the countries service by subscribing to params array to constantly change the ui upon change in params whiles completing the ui to match the subscribe country data

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from 'src/app/shared/countries.service';
+import { Country } from 'src/app/shared/types/country';
 
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
   styleUrls: ['./country-detail.component.css']
 })
-export class CountryDetailComponent {
+export class CountryDetailComponent implements OnInit {
+  country: Country | undefined;
 
+  constructor(private activateRoute: ActivatedRoute,
+    private countriesServices: CountriesService) { }
+
+  ngOnInit(): void {
+    this.activateRoute.params
+      .subscribe(params => {
+        this.country = this.countriesServices.findCountryById(params["id"]);
+      });
+  }
 }
