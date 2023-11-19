384d6b0a8a12988a57bfdae3cbad7dc99e295dcd
Bernard Teye <datssosh@gmail.com>
2023-11-18 22:10:44 -0700

    add country currency pipe to transform currencies details fetched from the api

---
+++ src/app/pages/country-detail/country-currency.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'countryCurrency'
+})
+export class CountryCurrencyPipe implements PipeTransform {
+
+  transform(value: any, ...args: unknown[]): any {
+    let currencies = <any[]>Object.values(value)
+    let output = "";
+
+    for(let i = 0; i < currencies.length; i++) {
+      if (i === currencies.length - 1)
+        output += currencies[i].name;
+      else
+        output += currencies[i].name + ", "
+    }
+
+    return output;
+  }
+
+}
