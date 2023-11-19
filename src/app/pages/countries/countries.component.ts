50fa21ddc645dc637150dacaea55b61b47bd3f7b
Bernard Teye <datssosh@gmail.com>
2023-11-18 20:43:02 -0700

    add support for filter and search using angular pipe concept

--- src/app/pages/countries/countries.component.ts
+++ src/app/pages/countries/countries.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CountriesService } from 'src/app/shared/countries.service';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.css']
 })
-export class CountriesComponent {
+export class CountriesComponent implements OnInit {
+  searchedCountry = "";
+  selectedFilter = "";
+  filters = ["Filter By Region", "Africa", "America", "Asia", "Europe", "Oceania"];
+  constructor(private countriesServices: CountriesService) { }
 
+  ngOnInit(): void {
+    this.countriesServices.loadCountries();
+  }
 }
--- src/app/pages/countries/country-list/country-list.component.ts
+++ src/app/pages/countries/country-list/country-list.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input } from '@angular/core';
 import { CountriesService } from 'src/app/shared/countries.service';
 import { Country } from 'src/app/shared/types/country';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-country-list',
   templateUrl: './country-list.component.html',
   styleUrls: ['./country-list.component.css']
 })
-export class CountryListComponent implements OnInit {
+export class CountryListComponent implements OnInit, OnDestroy {
+  @Input() filter = "";
+  @Input() searchTerm = "";
   countries: Country[] = [];
+  subscription: Subscription | undefined;
 
   constructor(private countriesService: CountriesService) { }
 
   ngOnInit() {
-    this.countries = this.countriesService.countryList;
+    this.subscription = this.countriesService.countriesChanged
+      .subscribe((fetchedCountries: Country[]) => {
+        this.countries = fetchedCountries.slice()
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
