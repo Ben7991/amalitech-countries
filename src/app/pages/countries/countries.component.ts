49936c404404c8a85cb7d8b13b624ac9c055c86d
Bernard Teye <datssosh@gmail.com>
2023-11-23 06:55:13 -0700

    improve search and filter behavior even when user moves away from the current page

--- src/app/pages/countries/countries.component.ts
+++ src/app/pages/countries/countries.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { CountriesService } from 'src/app/shared/countries.service';
 
 @Component({
@@ -6,13 +6,21 @@ import { CountriesService } from 'src/app/shared/countries.service';
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.css']
 })
-export class CountriesComponent implements OnInit {
+export class CountriesComponent implements DoCheck, OnInit {
   searchedCountry = "";
-  selectedFilter = "";
+  selectedFilter = "Filter By Region";
   filters = ["Filter By Region", "Africa", "America", "Asia", "Europe", "Oceania"];
+
   constructor(private countriesServices: CountriesService) { }
 
   ngOnInit(): void {
-    // this.countriesServices.loadCountries();
+    this.searchedCountry = localStorage.getItem("search-term") ? localStorage.getItem("search-term")! : "";
+    this.selectedFilter = localStorage.getItem("filter-term") ? localStorage.getItem("filter-term")! : "Filter By Region";
   }
+
+  ngDoCheck(): void {
+    localStorage.setItem("search-term", this.searchedCountry);
+    localStorage.setItem("filter-term", this.selectedFilter);
+  }
+
 }
